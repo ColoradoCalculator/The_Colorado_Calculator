434961dea0c4d1554d7b76be5ffd44169875574b
ironicsushi <yuli5565@colorado.edu>
2017-08-28 10:01:12 -0500

    started on javascript *added loop and listener function

--- colorado_calculator.js
+++ colorado_calculator.js
@@ -1,1 +1,26 @@
 "use strict";
+
+var numbersArray = document.getElementsByClassName("numbers");
+console.log(numbersArray);
+console.log(numbersArray[0]);
+
+// var zero = numbersArray.pop();
+// numbersArray.unshift(zero);
+// console.log(numbersArray);
+
+var enterNumberFunction = function () {
+    var first = document.getElementById("first_and_result");
+
+    for (var i = 0; i < 10; i++) {
+        if (i === numbersArray[i]) {
+            first.innerText = i;
+        }
+
+
+
+    }
+};
+
+
+
+// number.addEventListener("click", enterNumberFunction, false);
