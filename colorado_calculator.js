e68c8398b0bb4f92cd21072218adc6cb06e9a353
Uglygrayduck <Nicholasgswan@gmail.com>
2017-08-28 10:32:58 -0500

    trying to get our buttons to work

--- colorado_calculator.js
+++ colorado_calculator.js
@@ -3,24 +3,28 @@
 var numbersArray = document.getElementsByClassName("numbers");
 console.log(numbersArray);
 console.log(numbersArray[0]);
+console.log(numbersArray[0].innerText);
+console.log(numbersArray[0].innerHTML);
 
 // var zero = numbersArray.pop();
 // numbersArray.unshift(zero);
 // console.log(numbersArray);
-
-var enterNumberFunction = function () {
-    var first = document.getElementById("first_and_result");
-
-    for (var i = 0; i < 10; i++) {
-        if (i === numbersArray[i]) {
-            first.innerText = i;
-        }
-
-
-
-    }
-};
+var first = document.getElementById("first_and_result");
+function enterNumberFunction (i) {
+    first.innerText += numbersArray[i].innerText
+    // return function() {
+    //     first.innerText += numbersArray[i].innerText;
+    // }
+}
 
 
 
-// number.addEventListener("click", enterNumberFunction, false);
+
+    numbersArray.addEventListener("click", enterNumberFunction() );
+
+// first.innerText += numberToEnter;
+
+
+
+
+// numbersArray.addEventListener("click", enterNumberFunction, false);
